f126b1c98eaa5cfca98c42a9ce4bd14a032b8714
santiagopp510 <santiagopalacio007@gmail.com>
2026-08-26 10:17:15 -0500

    finalizacion

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 menu = """
-=== menu ===
-1. ver candidatos
-2. votar
-3. ver resultados
-4. salir
+      === menu ===
+ ||1. ver candidatos ||
+ ||2. votar          ||
+ ||3. ver resultados ||
+ ||4. salir          ||
 """
 continuar = "s"
 c1=0
@@ -12,30 +12,46 @@ c3=0
 while (continuar == "s"):
 
  print (menu)
+ print ("---------------------------")
  opcion=int(input("ingrese una opcion: ")) 
-
+ print ("---------------------------")
  match opcion:
   case 1:
    print ("c1")
    print ("c2")
    print ("c3")
+   print ("---------------------------")
+   continuar = input("¿Desea volver al menú? (s/n): ").lower()
   case 2:
     votacion = int(input("por quien deseas votar?? (1,2,3) "))
     if (votacion == 1):
+      print ("---------------------------")
+      print ("voto registrado")
       c1 = c1 + 1
     elif (votacion == 2):
+     print ("---------------------------")
+     print ("voto registrado")
      c2 = c2 + 1 
     elif (votacion == 3):
+     print ("---------------------------")
+     print ("voto registrado")
      c3 = c3 + 1
     else:
+     print ("---------------------------")
      print ("candidato no existe")
+     print ("---------------------------")
+    continuar = input("¿Desea volver al menú? (s/n): ").lower()
   case 3:
    print (c1)
    print (c2)
    print (c3)
+   continuar = input("¿Desea volver al menú? (s/n): ").lower()
   case 4:
    continuar = "n"
-   
+
+
+
+
 
 
 
